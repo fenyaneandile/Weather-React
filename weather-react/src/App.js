6abe6a8e0dda75937a7e05f3c7ed8496c1ce74e7
fenyaneandile <fenyaneandile@gmail.com>
2024-06-24 18:50:19 +0200

    Initial commit

---
+++ weather-react/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import WeatherSearch from "./WeatherSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather App</h1>
+        <WeatherSearch />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-react/src/WeatherSearch.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${
+        response.data.weather[0].icon
+      }@2x.png`,
+      description: response.data.weather[0].description
+    });
+}
+}
+
